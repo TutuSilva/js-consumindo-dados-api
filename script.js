1ef6e48ac2fcbb84729d00f4e35903d485246a8e
TutuSilva <160740774+TutuSilva@users.noreply.github.com>
2024-05-15 08:56:56 -0300

    tt

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-let consultaCEP = fetch("https://viacep.com.br/ws/01001000/json/").then(resposta => resposta.json());
+// let consultaCEP = fetch("https://viacep.com.br/ws/37975000/json/")
+//   .then((resposta) => resposta.json())
+//   .then((r) => {
+//     if (r.erro) {
+//       throw Error("Esse cep não existe");
+//     } else console.log(r);
+//   })
+//   .catch((erro) => console.log(erro))
+//   .finally((mensagem) => console.log("Processamento concluido"));
 
-console.log(consultaCEP);
+const soma = () => {
+  return number1 + number2;
+};
+console.log(soma);
